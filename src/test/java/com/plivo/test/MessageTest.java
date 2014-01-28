6c25b6464c9bb149f9ad320ae09786bf7a90208c
fiorenzo <fiorenzino@gmail.com>
2014-01-28 23:20:28 +0100

    aff junit test

--- src/test/java/com/plivo/test/MessageTest.java
+++ src/test/java/com/plivo/test/MessageTest.java
@@ -2,50 +2,53 @@ package com.plivo.test;
 
 import java.util.LinkedHashMap;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 import com.plivo.helper.api.client.RestAPI;
 import com.plivo.helper.api.response.call.Call;
 import com.plivo.helper.api.response.message.MessageResponse;
 import com.plivo.helper.exception.PlivoException;
 
-public class Test {
+public class MessageTest {
 
-	static String auth_id = "xxx";
-	static String auth_token = "xxxx";
+	static String auth_id = "xx";
+	static String auth_token = "xxxxx";
 
-	public static void main1(String[] args) {
+	@Test
+	public void testSingleMessage() {
 
 		RestAPI restAPI = new RestAPI(auth_id, auth_token, null);
 		LinkedHashMap<String, String> parameters = new LinkedHashMap<String, String>();
-		parameters.put("src", "xxx");
+		parameters.put("src", "x");
 		parameters.put("dst", "xx");
 		parameters.put("text", "ciao flower come va?");
-		parameters.put("url",
-				"http://xxx.rhcloud.com/statusCallback.jsf");
+		parameters.put("url", "http://xxx.rhcloud.com/statusCallback.jsf");
 		try {
 			MessageResponse result = restAPI.sendMessage(parameters);
 			for (String uid : result.messageUuids) {
-				System.out.println(uid);
+				Assert.assertNotNull(uid);
+				Assert.assertNull(result.error);
 			}
-			System.out.println();
 		} catch (PlivoException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void testMultiMessage() {
 
 		RestAPI restAPI = new RestAPI(auth_id, auth_token);
 		LinkedHashMap<String, String> parameters = new LinkedHashMap<String, String>();
-		parameters.put("from", "xx");
-		parameters.put("to", "xx<xx");
+		parameters.put("src", "xx");
+		parameters.put("dst", "xx<xx");
 		parameters.put("answer_url",
 				"http://xxxx.rhcloud.com/statusCallback.jsf");
 		try {
 			Call result = restAPI.makeCall(parameters);
-			System.out.println(result.requestUUID);
-			System.out.println(result.apiId);
-			System.out.println(result.message);
-			System.out.println(result.error);
+			Assert.assertNotNull(result.requestUUID);
+			Assert.assertNull(result.error);
 		} catch (PlivoException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
